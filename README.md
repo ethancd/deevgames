d88ae5758c7b8b138a4f394e6ef4f373512c70da
Ethan Dickinson <ethancdickinson@gmail.com>
2013-08-02 14:18:15 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# DeevGames
+
+## A site for my (theoretical) board game company.
+
+Will have a development blog, with comments, and an online implementation for
+one of my board games, *Ninja Tanks*.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeevGamesDevelopment::Application.config.secret_token = 'a67de70196eb7a744469fb64ff61155e06efccdabc8417de5314867cee885e104a4e967ae33d164149db6fbd05985f5b1713b312dca309a0021e79aabc31cc33'
