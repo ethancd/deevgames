9e03d3d6a71ad102e77415aaa68683d19501b8ac
Ethan Dickinson <ethancdickinson@gmail.com>
2013-08-18 15:44:28 -0400

    Edit seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
 require "csv"
 
-users = User.new(
+users = User.create([
   {username: "deev", email: "ethancdickinson@gmail.com", password: "12341234",
     avatar: File.new("#{Rails.root}/app/assets/images/avatars/dragon.png")},
   {username: "Ninja_Bot", email: "lalalafake@gmail.com", password: "12341234",
     avatar: File.new("#{Rails.root}/app/assets/images/avatars/ninjabot.png")},
-  {username: "demo", email: "demo@example.com", password: "12341234"})
+  {username: "demo", email: "demo@example.com", password: "12341234"}
+  ])
 
 users.first.admin = true
-
-users.each do |user| user.save! end
+users.first.save!
 
 CSV.foreach("db/seed_data/njtrules.csv", headers: true) do |rule|
   Rule.create(rule.to_hash)
